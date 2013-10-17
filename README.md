88513f26155842ec7178a69b2c5f192a3f2ead2a
Francesc Mallafre Lopez <franml87@gmail.com>
2013-10-17 15:09:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
 chrono.js
 =========
 
-Simple time counter / chronometer
+Simple time counter / chronometer by [weisk](https://github.com/weisk/)
+
+# Description
+
+chrono.js is a simple Javascript library to add real time tracking on your web app.
+
+
+## Usage
+
+Include the source code of chrono.js, and instantiate a new Chrono like this:
+
+```
+var timer = new Chrono();
+```
+OR
+```
+window.timer = new Chrono();
+```
+
+Then you can init the Chrono object with two optional settings: 
+* `timerElement`: a CSS Selector which will reflect the time as it passes,
+* `intervalSpan`: the frequency in milliseconds the timer will be updated at
+
+```
+timer.init({
+	timerElement: '#clock',
+	intervalSpan: 10
+});
+```
+
+## Properties
+
+* `status`: 1 (timer is running), 0 (timer is paused/stopped) .
+* `absoluteStart`: Absolute time in Date() format from which the Chrono first started.
+* `absoluteEnd`: Absolute time in Date() format from which the Chrono last stopped.
+* `elapsed`: Elapsed time in Milliseconds.
+* `startTime`: Last time in Date() format Chrono was started.
+* `pauseTime`: Last time in Date() format Chrono was paused.
+
+## Methods
+
+* `init([opts])`: Initialize the Chrono object setting the optional settings.
+* `start()`: Start tracking time.
+* `pause()`: Pause tracking time.
+* `stop()`: Same as pause().
+* `reset()`: Reset Chrono timer.
---
+++ chrono.js
@@ -0,0 +1,71 @@
+/*
+ * Domain Public by Francesc Mallafre Lopez (2013) 
+ * https://github.com/weisk/
+ */
+var Crono = function(){
+
+	var defaultSettings = { 
+		timerElement:null,
+		intervalSpan:10 
+	};	
+
+	var newdate = function() { 
+		return (new Date().getTime());
+	};
+	
+	var format = function(time) {
+		var mili = ('00'+(time % 1000)).substr(-3);
+		var secs = ('0'+(Math.floor(time/1000) % 60)).substr(-2);
+		var mins = Math.floor(time/60000);
+		return (mins+' : '+secs+' : '+mili);
+	};
+
+	var checkTime = function(that) {
+		var difftime = newdate() - that.startTime;
+		that.elapsed = difftime;
+		var diffstr = format(difftime);
+		$(that.settings.timerElement).val(diffstr);
+	};
+	
+	return {
+		init: function(opts){ 
+			var opts = opts || {};
+			this.settings = {};
+			$.extend(this.settings,defaultSettings,opts);
+			$(this.settings.timerElement).val('0 : 00 : 000');
+			this.elapsed = 0;
+			this.status = 0;
+		},
+		start: function() {
+			if(typeof this.status=="undefined") this.init();
+			this.status = 1;
+			if(!this.startTime) {
+				this.startTime = newdate();
+				this.absoluteStart = this.startTime;
+			}
+			if(this.pauseTime) this.startTime += newdate() - this.pauseTime;
+			var that = this;
+			this.interval = setInterval(function(){ checkTime(that); },this.settings.intervalSpan);
+		},
+		pause: function() {
+			if(typeof this.status!="undefined") {
+				this.status = 0;
+				if(this.interval) clearInterval(this.interval);
+				this.pauseTime = newdate();
+				this.absoluteEnd = this.pauseTime;
+			}
+		},
+		stop: function() {
+			this.pause()
+		},
+		reset: function() {
+			if(typeof this.status!="undefined") {
+				this.status = 0;
+				if(this.interval) clearInterval(this.interval);
+				if(this.startTime) delete this.startTime;
+				if(this.pauseTime) delete this.pauseTime;
+				$(this.settings.timerElement).val('0 : 00 : 000');
+			}
+		}
+	}
+}
